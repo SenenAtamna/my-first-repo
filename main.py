629088f8fed483dfff3185536c5aaa930ed08d90
senen <you@example.com>
2024-10-16 12:54:20 +0300

    adding print from programmer1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Student import Student
 from Employee import Employee
 
 student = Student("Mike", 30, "enginer", 1, 70)
+print("this is from programmer1")
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
