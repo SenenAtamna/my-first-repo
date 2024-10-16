41086c230645c32cef1707f9e12da4e5ad09027a
senen <you@example.com>
2024-10-16 12:23:38 +0300

    bug fixed missing information

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Mike", 30, "enginer", 1)
+student = Student("Mike", 30, "enginer", 1, 70)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
