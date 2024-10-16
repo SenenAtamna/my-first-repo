2a26c5564209771cf54e0f89d0b0c980a1c9672b
SenenAtamna <senenatamnii@gmail.com>
2024-10-16 12:52:19 +0300

    Update main.py
    add print for progmrammer 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Employee import Employee
 
 student = Student("Mike", 30, "enginer", 1, 70)
 # student.foo()
-
+print("this is from programmer 2 ")
 employee = Employee("John", 40, "Software Engineer", 45000)
 people = [student, employee]
 for person in people:
